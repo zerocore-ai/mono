1f0a20314a795b00c1676c585bb224c8e8b2fd3e
Stephen Akinyemi <appcypher@outlook.com>
2024-08-02 15:36:40 +0100

    fix: translucent window with rounded corners
    It is far from ideal as shadow isn't no longer as deep after remowing window decoration
    
    Webview background spills on rounded corner

--- prompt-bar/src/prompt_bar.rs
+++ prompt-bar/src/prompt_bar.rs
@@ -1,29 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{LogicalPosition, LogicalSize, WebviewBuilder, WebviewUrl, WindowBuilder};
+use tauri::Manager;
 use window_vibrancy::NSVisualEffectMaterial;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let width = 600.;
-            let height = 100.;
-            let window = WindowBuilder::new(app, "PromptBar")
-                .inner_size(width, height)
-                .decorations(false)
-                .build()
-                .expect("Failed to create window");
-
-            window.add_child(
-                WebviewBuilder::new("First", WebviewUrl::App(Default::default())).auto_resize(),
-                LogicalPosition::new(0., 0.),
-                LogicalSize::new(width, height),
-            )?;
+            let window = app.get_webview_window("main").unwrap();
 
             #[cfg(target_os = "macos")]
-            window_vibrancy::apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
-                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+            window_vibrancy::apply_vibrancy(
+                &window,
+                NSVisualEffectMaterial::HudWindow,
+                None,
+                Some(16.0),
+            )
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             #[cfg(target_os = "windows")]
             window_vibrancy::apply_blur(&window, Some((18, 18, 18, 125)))
